82e3a614952c1d54a8d15ab126d065b6809744e9
kydzoster <kydzoster@gmail.com>
2020-07-16 14:49:13 +0100

    created forms field for email comment form

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Comment
+
+
+class EmailPostForm(forms.Form):
+    # input text type field 
+    name = forms.CharField(max_length=25)
+    # 'email' and 'to' fields require valid email addresses
+    email = forms.EmailField()
+    to = forms.EmailField()
+    # comment field is optional to get validation
+    comments = forms.CharField(required=False, widget=forms.Textarea)
